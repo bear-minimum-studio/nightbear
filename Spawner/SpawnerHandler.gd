150dded247b03c1b81ad4cac8fb2212799c367ab
Firliminat <bobet.alexandre@gmail.com>
2021-04-25 01:01:43 +0200

    refacto making spawn delay accessible from spawnHandler node

--- Spawner/SpawnerHandler.gd
+++ Spawner/SpawnerHandler.gd
@@ -1,14 +1,17 @@
 extends Node2D
 
 export (PackedScene) var spawned_entity
+export (float) var spawn_delay = 0.5
 
 const DEFAULT_SPAWNED_ENTITY = preload("res://Projectiles/EnemyProjectile.tscn")
 
+onready var spawn_timer = $SpawnTimer
 onready var spawners = $SpawnerContainer.get_children()
 onready var nb_spawners = spawners.size()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spawn_timer.set_wait_time(spawn_delay)
 	randomize()
 	if !spawned_entity:
 		spawned_entity = DEFAULT_SPAWNED_ENTITY
