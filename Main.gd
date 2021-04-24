193b76a87c10af5f2dd0361385c3d531e8d4bf50
Pierre Poupin <pierrep@bam.tech>
2021-04-24 21:05:41 +0200

    add Game logic to Main

--- Main.gd
+++ Main.gd
@@ -10,6 +10,21 @@ onready var camera2 = get_node(camera2_path)
 onready var world1 = get_node(world1_path)
 onready var world2 = get_node(world2_path)
 
+const wall = preload("res://Obstacles/Wall.tscn")
+
 func _ready():
 	camera1.target = world1
 	camera2.target = world2
+
+	var players = get_tree().get_nodes_in_group("player")
+	for player in players:
+		player.connect("build", self, "_build")
+
+func _build(id: int, t:Transform2D):
+	var new_wall = wall.instance()
+	new_wall.transform = t
+
+	if (id  == 1):
+		world2.add_child(new_wall)
+	elif (id  == 2):
+		world1.add_child(new_wall)
