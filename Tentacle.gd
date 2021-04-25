21838de8d3774f9f7e20166123ab390efce60741
Martin Castel <martincastel.mc@gmail.com>
2021-04-25 23:05:25 +0200

    ADD: Tentacle

---
+++ Tentacle.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+onready var sprite = $Sprite
+onready var animation_player = $AnimationPlayer
+
+var size = 1
+
+func _ready():
+	pass
+
+func _input(event):
+	if (event.is_action_pressed("P1_walk_left")):
+		grow()
+
+func grow():
+	size = min(size+1, 4)
+	animation_player.play("Wave0"+String(size))
+	
