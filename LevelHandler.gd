8418f25f5bf6e6f69304232427f251db3b546670
Alexandre BOBET <bobet.alexandre@gmail.com>
2021-04-26 01:37:49 +0200

    refacto level handler & tweak level params

--- LevelHandler.gd
+++ LevelHandler.gd
@@ -4,24 +4,13 @@ var worlds
 
 onready var burst_start_timer = $BurstStartTimer
 
-var empty_wave = [
-	{
-		"world_indexes": [],
-		"spawn_type": Burst.SpawnType.Ally,
-		"spawn_delay": -1,
-		"burst_duration": -1,
-		"burst_start_delay": 30,
-		"burst_sides": []
-	}
-]
-
 var wave1 = [
 	{
 		"world_indexes": [0, 1],
 		"spawn_type": Burst.SpawnType.Enemy,
 		"spawn_delay": 0.05,
 		"burst_duration": 2,
-		"burst_start_delay": 4,
+		"next_burst_start_delay": 15,
 		"burst_sides": [SpawnHandler.Sides.Top]
 	},
 	{
@@ -29,7 +18,7 @@ var wave1 = [
 		"spawn_type": Burst.SpawnType.Enemy,
 		"spawn_delay": 0.03,
 		"burst_duration": 1,
-		"burst_start_delay": 0.5,
+		"next_burst_start_delay": 0.5,
 		"burst_sides": [SpawnHandler.Sides.Left]
 	},
 	{
@@ -37,23 +26,31 @@ var wave1 = [
 		"spawn_type": Burst.SpawnType.Enemy,
 		"spawn_delay": 0.03,
 		"burst_duration": 1,
-		"burst_start_delay": 0.5,
+		"next_burst_start_delay": 0,
 		"burst_sides": [SpawnHandler.Sides.Right]
 	},
 	{
-		"world_indexes": [0, 1],
+		"world_indexes": [0],
 		"spawn_type": Burst.SpawnType.Ally,
 		"spawn_delay": 0,
 		"burst_duration": 1,
-		"burst_start_delay": 0,
-		"burst_sides": [SpawnHandler.Sides.Left, SpawnHandler.Sides.Top, SpawnHandler.Sides.Right, SpawnHandler.Sides.Bottom]
+		"next_burst_start_delay": 0,
+		"burst_sides": [SpawnHandler.Sides.Right]
+	},
+	{
+		"world_indexes": [1],
+		"spawn_type": Burst.SpawnType.Ally,
+		"spawn_delay": 0,
+		"burst_duration": 1,
+		"next_burst_start_delay": 15,
+		"burst_sides": [SpawnHandler.Sides.Left]
 	},
 	{
 		"world_indexes": [0, 1],
 		"spawn_type": Burst.SpawnType.Enemy,
 		"spawn_delay": 0.03,
 		"burst_duration": 1,
-		"burst_start_delay": 4,
+		"next_burst_start_delay": 0,
 		"burst_sides": [SpawnHandler.Sides.Top]
 	},
 	{
@@ -61,15 +58,15 @@ var wave1 = [
 		"spawn_type": Burst.SpawnType.Ally,
 		"spawn_delay": 0,
 		"burst_duration": 1,
-		"burst_start_delay": 0,
-		"burst_sides": [SpawnHandler.Sides.Left, SpawnHandler.Sides.Top, SpawnHandler.Sides.Right, SpawnHandler.Sides.Bottom]
+		"next_burst_start_delay": 15,
+		"burst_sides": [SpawnHandler.Sides.Bottom]
 	},
 	{
 		"world_indexes": [0],
 		"spawn_type": Burst.SpawnType.Enemy,
 		"spawn_delay": 0.03,
 		"burst_duration": 1,
-		"burst_start_delay": 0,
+		"next_burst_start_delay": 0,
 		"burst_sides": [SpawnHandler.Sides.Left]
 	},
 	{
@@ -77,13 +74,12 @@ var wave1 = [
 		"spawn_type": Burst.SpawnType.Enemy,
 		"spawn_delay": 0.03,
 		"burst_duration": 1,
-		"burst_start_delay": 0,
+		"next_burst_start_delay": 0,
 		"burst_sides": [SpawnHandler.Sides.Right]
 	}
 ]
 
-#var full_level = [empty_wave]
-var full_level = [wave1, empty_wave, wave1]
+var full_level
 
 var current_level
 var wave_index
@@ -94,45 +90,62 @@ func initialize(father_worlds):
 	worlds = father_worlds
 	for world in worlds:
 		world.spawner_handler.connect("burst_ended", self, "_on_burst_ended")
+		
+	full_level = [wave1, _create_empty_wave(35), wave1]
 
 func start():
-	play_level(full_level)
+	_play_level(full_level)
 
-func play_level(level):
+func _create_empty_burst_description(next_burst_start_delay):
+	var empty_burst_description = {
+		"world_indexes": [],
+		"spawn_type": Burst.SpawnType.Ally,
+		"spawn_delay": -1,
+		"burst_duration": -1,
+		"next_burst_start_delay": next_burst_start_delay,
+		"burst_sides": []
+	}
+	return empty_burst_description
+
+func _create_empty_wave(next_wave_start_delay):
+	var empty_wave = [_create_empty_burst_description(next_wave_start_delay)]
+	return empty_wave
+
+func _play_level(level):
 	print("Level started")
 	current_level = level
 	wave_index = 0
 	var wave = level[wave_index]
-	play_wave(wave)
+	_play_wave(wave)
 
-func level_ended():
+func _level_ended():
 	print("Level ended")
 	burst_start_timer.stop()
 
-func next_wave():
+func _next_wave():
 	wave_index += 1
 	if wave_index < current_level.size():
 		var wave = current_level[wave_index]
-		play_wave(wave)
+		_play_wave(wave)
 	else:
-		level_ended()
+		_level_ended()
 
-func play_wave(wave):
+func _play_wave(wave):
 	print("Wave %d started" % wave_index)
 	current_wave = wave
 	burst_index = 0
 	var burst_description = wave[burst_index]
-	play_burst(burst_description)
+	_play_burst(burst_description)
 
-func next_burst():
+func _next_burst():
 	burst_index += 1
 	if burst_index < current_wave.size():
 		var burst_description = current_wave[burst_index]
-		play_burst(burst_description)
+		_play_burst(burst_description)
 	else:
-		next_wave()
+		_next_wave()
 
-func play_burst(burst_description):
+func _play_burst(burst_description):
 	print("Burst %d started" % burst_index)
 	print(burst_description.spawn_type)
 	for world_index in burst_description.world_indexes:
@@ -143,8 +156,8 @@ func play_burst(burst_description):
 			burst_description.spawn_delay,
 			burst_description.burst_duration,
 			burst_description.burst_sides)
-	print("Burst start timer started for : %f" % burst_description.burst_start_delay)
-	burst_start_timer.start(burst_description.burst_start_delay)
+	print("Burst start timer started for : %f" % burst_description.next_burst_start_delay)
+	burst_start_timer.start(burst_description.next_burst_start_delay)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -154,4 +167,4 @@ func _on_burst_ended(_burst):
 	pass
 
 func _on_BurstStartTimer_timeout():
-	next_burst()
+	_next_burst()
