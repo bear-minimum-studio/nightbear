70ede62d1fb183994ac80cc61ba95aedd4fca0ad
Martin Castel <martincastel.mc@gmail.com>
2021-04-25 16:51:00 +0200

    new run animation

--- Player/PlayerSprite.gd
+++ Player/PlayerSprite.gd
@@ -5,8 +5,8 @@ export(NodePath) var player_path
 onready var player = get_node(player_path)
 
 const body_sprites = {
-	1: preload("res://Player/bear_1_R.png"),
-	2: preload("res://Player/bear_2_R.png"),
+	1: preload("res://Player/bear1.png"),
+	2: preload("res://Player/bear2.png"),
 }
 
 var id = 1
@@ -15,5 +15,5 @@ func initialize(father_id: int):
 	self.texture = body_sprites[father_id]
 
 func set_orientation(horizontal_speed: float):
-	var orientation = sign(horizontal_speed) * abs(self.scale.x)
+	var orientation = -sign(horizontal_speed) * abs(self.scale.x)
 	self.scale.x = orientation
