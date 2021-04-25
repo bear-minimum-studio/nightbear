5c0cf417769eb066c7af9cb13149d17720c54829
Firliminat <bobet.alexandre@gmail.com>
2021-04-25 20:49:59 +0200

    add level management

--- Game.gd
+++ Game.gd
@@ -16,17 +16,93 @@ onready var dream_caught_text = $DreamCaughtText
 
 var dreams_caught = 0
 
-var current_wave = []
-var burst_index = 0
+onready var empty_wave = [
+	{
+		"spawner_handler": ally_projectile_spawner_handlers[0],
+		"spawn_delay": 10,
+		"burst_duration": 10,
+		"burst_start_delay": 10,
+		"burst_sides": []
+	}
+]
+
+onready var wave1 = [
+	{
+		"spawner_handler": ally_projectile_spawner_handlers[0],
+		"spawn_delay": 0.01,
+		"burst_duration": 0.015,
+		"burst_start_delay": 0.015,
+		"burst_sides": [SpawnHandler.Sides.Top]
+	},
+	{
+		"spawner_handler": enemy_projectile_spawner_handlers[0],
+		"spawn_delay": 0.03,
+		"burst_duration": 3,
+		"burst_start_delay": 2,
+		"burst_sides": [SpawnHandler.Sides.Left]
+	}
+]
+
+onready var wave2 = [
+	{
+		"spawner_handler": ally_projectile_spawner_handlers[0],
+		"spawn_delay": 0.01,
+		"burst_duration": 0.015,
+		"burst_start_delay": 0.015,
+		"burst_sides": [SpawnHandler.Sides.Top]
+	},
+	{
+		"spawner_handler": enemy_projectile_spawner_handlers[0],
+		"spawn_delay": 0.03,
+		"burst_duration": 3,
+		"burst_start_delay": 2,
+		"burst_sides": [SpawnHandler.Sides.Left]
+	}
+]
+
+onready var test_level = [wave1, empty_wave, wave2]
+
+var current_level
+var wave_index
+var current_wave
+var burst_index
 var current_burst
 
+func play_level(level):
+	print("Level started")
+	current_level = level
+	wave_index = 0
+	var wave = level[wave_index]
+	play_wave(wave)
+
+func level_ended():
+	print("Level ended")
+
+func next_wave():
+		wave_index += 1
+		if wave_index < current_level.size():
+			var wave = current_level[wave_index]
+			play_wave(wave)
+		else:
+			level_ended()
+
 func play_wave(wave):
+	print("Wave %d started" % wave_index)
 	current_wave = wave
 	burst_index = 0
 	var burst_description = wave[burst_index]
 	current_burst = create_burst(burst_description)
 
+func next_burst():
+	burst_index += 1
+	if burst_index < current_wave.size():
+		var burst_description = current_wave[burst_index]
+		current_burst = create_burst(burst_description)
+	else:
+		next_wave()
+
 func create_burst(burst_description):
+	print("Burst %d created" % burst_index)
 	var burst = burst_description.spawner_handler.start_burst(
 		burst_description.spawn_delay,
 		burst_description.burst_duration,
@@ -48,26 +124,9 @@ func _ready():
 		player.connect("build", self, "_build")
 		player.connect("player_dead", self, "_player_dead")
 		player.connect("player_moved", self, "_move_player_shade")
-		
-	var wave  = [
-		{
-			"spawner_handler": ally_projectile_spawner_handlers[0],
-			"spawn_delay": 0.06,
-			"burst_duration": 6,
-			"burst_start_delay": 1.5,
-			"burst_sides": [SpawnHandler.Sides.Top]
-		},
-		{
-			"spawner_handler": enemy_projectile_spawner_handlers[0],
-			"spawn_delay": 0.03,
-			"burst_duration": 3,
-			"burst_start_delay": 2,
-			"burst_sides": [SpawnHandler.Sides.Left]
-		}
-	]
 	
-	play_wave(wave)
-	
+	play_level(test_level)
+
 func _build(id: int, t:Transform2D):
 	var new_wall = Parameters.GAME_WALL.instance()
 	new_wall.transform.origin = t.origin
@@ -107,9 +166,4 @@ func _on_burst_ended():
 	print("Burst ended")
 
 func _on_burst_StartDelayTimer_timeout():
-	burst_index += 1
-	if burst_index < current_wave.size():
-		var burst_description = current_wave[burst_index]
-		current_burst = create_burst(burst_description)
-	else:
-		pass
+	next_burst()
