48f396da7ea9ca0aae9c0c49a8fda1b49ede8f1e
Pierre Poupin <pierrep@bam.tech>
2021-04-25 20:40:45 +0200

    add paw as player shade

--- PlayerShade.gd
+++ PlayerShade.gd
@@ -2,8 +2,14 @@ extends Sprite
 
 var id = 1
 
+const body_sprites = {
+	1: preload("res://Player/paw2.png"),
+	2: preload("res://Player/paw1.png"),
+}
+
 func initialize(player_id: int):
-	id = player_id # Replace with function body.
+	id = player_id
+	self.texture = body_sprites[player_id]
 
 func move_shade(position: Vector2):
 	transform.origin = position
