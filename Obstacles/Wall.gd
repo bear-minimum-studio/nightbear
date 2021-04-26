29b27bd144ed62e581abbdc76afd7ce3dd02871f
legodaig <gael.le-godais@gipsa-lab.grenoble-inp.fr>
2021-04-26 11:42:41 +0200

    implement shield SFX

--- Obstacles/Wall.gd
+++ Obstacles/Wall.gd
@@ -5,6 +5,10 @@ class_name Wall
 onready var animated_sprite = $AnimatedSprite
 onready var lifebar = $ProgressBar
 onready var timer = $Timer
+onready var cast_fx = $CastFX
+onready var hit_fx = $HitFX
+onready var destroyed_fx = $DestroyedFX
+
 
 const WALL_NUMBER_OF_SPRITES = 1
 
@@ -12,14 +16,17 @@ func _ready():
 	timer.start(Parameters.WALL_MAX_LIFETIME)
 	randomize()
 	animated_sprite.frame = randi() % WALL_NUMBER_OF_SPRITES
-
+	cast_fx.play()
+	
 func _on_Timer_timeout():
+	destroyed_fx.play()
 	self.queue_free()
 
 func _physics_process(_delta):
 	_process_color()
 
 func hit():
+	hit_fx.play()
 	_set_timer()
 
 func _process_color():
