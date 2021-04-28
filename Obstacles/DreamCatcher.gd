ba608f494a1dc45f9ba6dd925e3b968c227a766a
Pierre Poupin <pierrep@bam.tech>
2021-04-29 00:46:19 +0200

    remove dead code

--- Obstacles/DreamCatcher.gd
+++ Obstacles/DreamCatcher.gd
@@ -4,17 +4,12 @@ class_name DreamCatcher
 
 var health_points = 1
 
-onready var animated_sprite = $AnimatedSprite
 onready var timer = $Timer
 onready var cast_fx = $CastFX
 onready var catch_fx = $CatchFX
 
-const DREAM_CATCHER_NUMBER_OF_SPRITES = 1
-
 func _ready():
 	timer.start(Parameters.DREAM_CATCHER_MAX_LIFETIME)
-	randomize()
-	animated_sprite.frame = randi() % DREAM_CATCHER_NUMBER_OF_SPRITES
 	cast_fx.play()
 
 func _on_Timer_timeout():
--- Obstacles/Wall.gd
+++ Obstacles/Wall.gd
@@ -2,20 +2,14 @@ extends StaticBody2D
 
 class_name Wall
 
-onready var animated_sprite = $AnimatedSprite
 onready var lifebar = $ProgressBar
 onready var timer = $Timer
 onready var cast_fx = $CastFX
 onready var hit_fx = $HitFX
 onready var destroyed_fx = $DestroyedFX
 
-
-const WALL_NUMBER_OF_SPRITES = 1
-
 func _ready():
 	timer.start(Parameters.WALL_MAX_LIFETIME)
-	randomize()
-	animated_sprite.frame = randi() % WALL_NUMBER_OF_SPRITES
 	cast_fx.play()
 	
 func _on_Timer_timeout():
