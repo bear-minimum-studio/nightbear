c5b6be7fdc49772c486757f41d40f35533fca7ce
alexandre.bobet <alexandre.bobet@ezygain.com>
2022-09-25 23:16:24 +0200

    fix(godot4): fix some animations

--- World/LevelManagement/Resources/BurstResource.gd
+++ World/LevelManagement/Resources/BurstResource.gd
@@ -16,31 +16,22 @@ class_name BurstResource
 
 var world_indexes = [] :
 	get:
-		return world_indexes # TODOConverter40 Copy here content of _get_world_indexes 
-	set(mod_value):
-		mod_value  # TODOConverter40  Non existent set function
+		var adapted_world_indexes = []
+		if world_1:
+			adapted_world_indexes.append(0)
+		if world_2:
+			adapted_world_indexes.append(1)
+		return adapted_world_indexes
+
 var sides = [] :
 	get:
-		return sides # TODOConverter40 Copy here content of _get_sides_array 
-	set(mod_value):
-		mod_value  # TODOConverter40  Non existent set function
-
-func _get_world_indexes() -> Array:
-	var adapted_world_indexes = []
-	if world_1:
-		adapted_world_indexes.append(0)
-	if world_2:
-		adapted_world_indexes.append(1)
-	return adapted_world_indexes
-
-func _get_sides_array() -> Array:
-	var adapted_sides_array = []
-	if left:
-		adapted_sides_array.append(SpawnHandler.Sides.Left)
-	if right:
-		adapted_sides_array.append(SpawnHandler.Sides.Right)
-	if top:
-		adapted_sides_array.append(SpawnHandler.Sides.Top)
-	if bottom:
-		adapted_sides_array.append(SpawnHandler.Sides.Bottom)
-	return adapted_sides_array
+		var adapted_sides_array = []
+		if left:
+			adapted_sides_array.append(SpawnHandler.Sides.Left)
+		if right:
+			adapted_sides_array.append(SpawnHandler.Sides.Right)
+		if top:
+			adapted_sides_array.append(SpawnHandler.Sides.Top)
+		if bottom:
+			adapted_sides_array.append(SpawnHandler.Sides.Bottom)
+		return adapted_sides_array
