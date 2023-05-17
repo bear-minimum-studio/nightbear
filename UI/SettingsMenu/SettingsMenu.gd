b6b92a2ba577af11e0ffe61ef97d4ecd52709435
zelv <contact@elv-studio.com>
2023-05-17 16:36:09 +0200

    feat: add fullscreen checkbox to settings

--- UI/SettingsMenu/SettingsMenu.gd
+++ UI/SettingsMenu/SettingsMenu.gd
@@ -2,4 +2,15 @@ extends Control
 
 class_name  SettingsMenu
 
+@onready var check_button = $MarginContainer/VBoxContainer/CheckButton
+
 @onready var exits_dict = {}
+
+
+func _ready():
+	check_button.button_pressed = Settings.fullscreen
+
+
+func _on_check_button_toggled(button_pressed):
+	Settings.fullscreen = button_pressed
+
