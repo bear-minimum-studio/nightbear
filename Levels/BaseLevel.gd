8336ae79a211aa34e33d82bcd24ec4f84dc44b1e
Gahel <music@gahel.fr>
2024-01-11 03:44:27 +0100

    fix: world reloading in multiplayer

--- Levels/BaseLevel.gd
+++ Levels/BaseLevel.gd
@@ -4,6 +4,9 @@ extends Control
 class_name BaseLevel
 
 
+signal client_world_reloaded
+signal world_reloaded
+
 ## Has to be a BaseWorld
 @export var world_scene : PackedScene :
 	set(value):
@@ -20,13 +23,16 @@ class_name BaseLevel
 
 var world : BaseWorld
 
+var is_client_world_reloaded := false
+
+## copy of the player_ids and their matching peer_ids as last set by Game 
+var player_authorities := {}
+
 
 func _ready():
 	anchors_preset = PRESET_FULL_RECT
 	if not Engine.is_editor_hint():
-		world = world_scene.instantiate()
-		split_screen.world = world # world is added as a child of splitscreen's viewport0
-		world.player_dead.connect(_on_player_dead)
+		load_world()
 
 
 func _get_configuration_warnings():
@@ -38,6 +44,12 @@ func _get_configuration_warnings():
 	return warning
 
 
+func load_world():
+	world = world_scene.instantiate()
+	split_screen.world = world # world is added as a child of splitscreen's viewport0
+	world.player_dead.connect(_on_player_dead)
+
+
 @rpc("authority", "call_local", "reliable")
 func start():
 	world.start()
@@ -45,15 +57,40 @@ func start():
 
 @rpc("authority", "call_local", "reliable")
 func reload():
-	print('reload level')
+	print('Reloading world')
 	world.queue_free()
-	_ready()
-	world.start()
+	await world.tree_exited
+	load_world()
+	
+	if not is_multiplayer_authority():
+		notify_client_world_reloaded.rpc()
+		
+	if not NetworkTools.local_multiplayer and is_multiplayer_authority():
+		if not is_client_world_reloaded:
+			await client_world_reloaded
+		is_client_world_reloaded = false
+		
+		for player_id in player_authorities.keys():
+			set_player_authority.rpc(player_authorities[player_id], player_id)
+	
+	if is_multiplayer_authority():
+		world_reloaded.emit()
 
 
-func _on_player_dead(player_id):
-	reload()
+@rpc("any_peer", "call_remote", "reliable")
+func notify_client_world_reloaded():
+	is_client_world_reloaded = true
+	client_world_reloaded.emit()
 
 
+func _on_player_dead(player_id: int):
+	if is_multiplayer_authority():
+		reload.rpc()
+		await world_reloaded
+		start.rpc()
+
+
+@rpc("authority", "call_local", "reliable")
 func set_player_authority(peer_id: int, player_id: int):
+	player_authorities[player_id] = peer_id
 	world.set_player_authority(peer_id, player_id)
